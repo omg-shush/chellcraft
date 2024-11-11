0229467e54faee49a2a2c2b82399b7fadb27b464
omg-shush <possiblychell@gmail.com>
2024-11-10 23:18:46 -0800

    Add boilerplate

---
+++ src/main/java/com/chellrose/chellcraft/ChellCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.chellrose.chellcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChellCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
