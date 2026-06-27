0d0348335e3aa76473ecd5d359b97082685d55ea
omg-shush <possiblychell@gmail.com>
2026-06-27 08:36:25 -0700

    Add wrench armor stand functionality

--- src/main/java/com/chellrose/chellcraft/ChellCraft.java
+++ src/main/java/com/chellrose/chellcraft/ChellCraft.java
@@ -19,6 +19,7 @@ import com.chellrose.chellcraft.features.invis.ListenerMakeInvis;
 import com.chellrose.chellcraft.features.invis.ListenerRemoveItemFromInvis;
 import com.chellrose.chellcraft.features.invis.ListenerWashInvis;
 import com.chellrose.chellcraft.features.sit.ListenerSit;
+import com.chellrose.chellcraft.features.wrench.ListenerWrenchArmorStand;
 import com.chellrose.chellcraft.features.wrench.ListenerWrenchPlace;
 
 public class ChellCraft implements ModInitializer {
@@ -50,6 +51,7 @@ public class ChellCraft implements ModInitializer {
 		@NonNull ModContainer thisContainer = Objects.requireNonNull(FabricLoader.getInstance().getModContainer(MOD_ID).get());
 		ResourceLoader.registerBuiltinPack(ChellCraftDataGenerator.WRENCH_RECIPE_PACK_ID, thisContainer, PackActivationType.ALWAYS_ENABLED);
 		new ListenerWrenchPlace();
+		new ListenerWrenchArmorStand();
 
 		LOGGER.info("Ready for liftoff!");
 	}
---
+++ src/main/java/com/chellrose/chellcraft/features/wrench/ListenerWrenchArmorStand.java
@@ -0,0 +1,52 @@
+package com.chellrose.chellcraft.features.wrench;
+
+import java.util.Objects;
+
+import org.jspecify.annotations.NonNull;
+
+import com.chellrose.chellcraft.features.armorstand.ArmorStandPose;
+
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.decoration.ArmorStand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+
+public class ListenerWrenchArmorStand {
+    public ListenerWrenchArmorStand() {
+        UseEntityCallback.EVENT.register(this::entity);
+    }
+
+    private @NonNull InteractionResult entity(Player player, Level level, @NonNull InteractionHand hand, Entity entity, EntityHitResult hitResult) {
+        ItemStack stack = player.getItemInHand(hand);
+        boolean mainHand = hand.asEquipmentSlot().equals(EquipmentSlot.MAINHAND);
+        boolean isWrench = WrenchItem.isWrench(stack);
+        boolean isArmorStand = entity.getType().equals(EntityType.ARMOR_STAND);
+        boolean isSneaking = player.isShiftKeyDown();
+        if (mainHand && isWrench && isArmorStand && isSneaking) {
+            // Cycle to next armor stand pose
+            ArmorStand armorStand = (ArmorStand) entity;
+            ArmorStandPose currentPose = new ArmorStandPose(armorStand);
+            int nextIndex = currentPose.index() + 1;
+            ArmorStandPose nextPose = ArmorStandPose.fromIndex(nextIndex);
+            nextPose.apply(armorStand);
+            // Can't damage armor stand in creative mode without immediately breaking
+            if (player.gameMode() != null && !Objects.requireNonNull(player.gameMode()).isCreative()) {
+                armorStand.hurtServer((ServerLevel) level, level.damageSources().playerAttack(player), 0.0f);
+                armorStand.lastHit = level.getGameTime() - 100L; // Reset the last hit time to prevent armor stand being broken
+            }
+            armorStand.playSound(SoundEvents.ARMOR_STAND_HIT);
+
+            return InteractionResult.CONSUME;
+        }
+        return InteractionResult.PASS;
+    }
+}
